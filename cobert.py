897e8b81703ed5b93722427e166fe8a2b2c69bb8
Tito Bouzout <tito.bouzout@gmail.com>
2023-05-29 10:29:27 -0300

    Update

--- cobert.py
+++ cobert.py
@@ -3,8 +3,8 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-model = dir_path + "/codebert-base-mlm"
-model = dir_path + "/codebert-javascript"
+# model = dir_path + "/codebert-base-mlm"
+model = dir_path + "/models/codebert-javascript"
 
 fill_mask = pipeline("fill-mask", model=model, tokenizer=model, device="cuda:0")
 
--- server.py
+++ server.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 # codehint api
 
-from codehint import codehint
+from cobert import codehint
 
 
 @app.route("/codehint", methods=["POST"])
 def apicodehint():
     code = request.json.get("code")
     result = codehint(code)
-    return json.dumps(result, indent=2)
+    return jsonify(result)
 
 
 # codesyntax hint api
